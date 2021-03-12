0f33a4cf77730890653de4644c2eb12e861e42b7
Tyler Maatrix <tylermaatrix@gmail.com>
2021-03-12 05:12:51 -0500

    added hero and styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Home from "./components/pages/Home";
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact></Route>
+          <Route path="/" exact component={Home}></Route>
         </Switch>
       </Router>
     </>
